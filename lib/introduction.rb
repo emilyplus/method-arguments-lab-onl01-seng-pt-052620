e2257327ca3e3ae6dccf3d82e58ed6f5e2822f8d
Emily Jones <emjones829@gmail.com>
2020-06-02 21:48:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I prefer to code with #{language}."
 end
+introduction("Emily", "Python")
